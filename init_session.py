f1f37c81c415f2810a96d0642eb5019efdcf6526
madhumithan8164 <madhumithan.s.official@gmail.com>
2025-07-03 12:41:17 +0530

    Add dotenv support and update requirements

--- init_session.py
+++ init_session.py
@@ -1,6 +1,7 @@
 import os
 from telethon.sync import TelegramClient
-
+from dotenv import load_dotenv
+load_dotenv()
 api_id = int(os.environ.get('API_ID'))
 api_hash = os.environ.get('API_HASH')
 phone = os.environ.get('PHONE_NUMBER')
