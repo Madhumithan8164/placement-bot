345cd7df4b1ce46016b79e7e2b2444116a66d843
madhumithan8164 <madhumithan.s.official@gmail.com>
2025-07-03 12:22:04 +0530

    Update main.py to use env vars and add init_session.py

---
+++ init_session.py
@@ -0,0 +1,9 @@
+import os
+from telethon.sync import TelegramClient
+
+api_id = int(os.environ.get('API_ID'))
+api_hash = os.environ.get('API_HASH')
+phone = os.environ.get('PHONE_NUMBER')
+
+with TelegramClient('session_name', api_id, api_hash) as client:
+    client.start(phone)
